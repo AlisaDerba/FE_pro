cb72d330f3e085b0d19f0b9538501ef5b7b4f76d
Аліса Дерба <alisa.derba@brocard.ua>
2018-05-17 14:38:38 +0300

    change platform game

--- DOM-practice/platform_game/main.js
+++ DOM-practice/platform_game/main.js
@@ -1,28 +1,75 @@
-window.onload = function(){
-    
-var block = document.querySelector('.block');
-var x;
-var y;
+window.onload = function() {
 
-block.addEventListener('mousedown', startAction);
+	var block = document.querySelector('.block') 	
+	document.addEventListener('keydown', action);
+	document.addEventListener('keyup', sit);
+	console.dir(block);
 
-function startAction(event){
-   x = event.offsetX;
-   y = event.offsetY;
-  document.addEventListener('mousemove', changePosition);
-}
+	var step = 40;
+	var h = 100;
+	var direction = 1;
+	budder = 2 + 'px';
+	
 
-  function changePosition(event){
+	function action(event){
+		// console.log(event);
+		//console.dir(window)
 
-    block.style.left = event.pageX - x + 'px'; 
-    block.style.top = event.pageY - y + 'px';
- 
-} 
-    
-document.addEventListener('mouseup', stopAction);
+		if(event.key == 'ArrowLeft' && block.offsetLeft >= 0) {
+			block.style.left = block.offsetLeft - step + 'px';
+		}
 
-    function stopAction(){
-        document.removeEventListener('mousemove', changePosition);
-    }
+		if(event.key == 'ArrowRight' &&  block.offsetLeft + block.offsetWidth <= window.innerWidth) {
+			block.style.left = block.offsetLeft + step + 'px';
+		}
+		
+		if(event.key == 'ArrowUp' && !event.ctrlKey && block.offsetTop >= 0) {
+			block.style.top = block.offsetTop - step + 'px';
+		}
 
+		if(event.key == 'ArrowDown' && !event.ctrlKey && block.offsetTop + block.offsetHeight <= window.innerHeight ) {
+			block.style.top = block.offsetTop + step + 'px';
+		}
+	    if(event.key == ' ' && !event.ctrlKey) {
+		var timer = setInterval(jump, 45);
+		}
+
+		var up = block.offsetTop - h;
+
+	    function jump() {
+
+			if((block.offsetTop <= up)) {
+				direction = -direction;
+		   }
+
+			 block.style.top = block.offsetTop - (step * direction) + 'px';
+
+			
+			if(block.offsetTop == up + h){
+				 direction = -direction;
+				clearInterval(timer)
+			}
+
+		}
+	
+		if(event.key == 'Control') {
+			
+			block.style.height = block.clientHeight *.6 + 'px';
+			block.style.width = block.clientWidth * 1.15 + 'px';
+			
+		}
+		
+		
+	}
+
+
+function sit(event){
+
+	if(event.key == 'Control') {
+			
+		block.style.height = 100 + 'px';
+		block.style.width = 100 + 'px';
+		
+		}
+	}
 }
